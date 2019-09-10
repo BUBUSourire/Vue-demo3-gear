a73b9208fd22ec088dab92f67f57e47636d5e703
Sourire <953810390@qq.com>
2019-09-10 23:30:42 +0800

    创建 icon group

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Button from './Button'
 import Icon from "./Icon";
+import Button_group from "./Button_group";
 
 // Vue.component('g-button',Button)
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
+Vue.component('g-button-group',Button_group)
 
 new Vue({
     el:'#app',
