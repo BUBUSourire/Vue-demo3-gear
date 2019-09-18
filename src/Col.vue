bce5067d3e12945161f2de9074ccecb6cb87af57
Sourire <953810390@qq.com>
2019-09-18 15:37:21 +0800

    基本实现row和col的功能

--- src/Col.vue
+++ src/Col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[span &&`col-${span}`,offset &&`offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"> <!--语法记忆：若offset存在，则执行后面的代码-->
+        <div style="border: 1px solid green">
         <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -8,8 +11,16 @@
     export default {
         name: "g-col",
         props: {
-            span: {
+            span: { //所占大小
                 type: [Number, String]
+            },
+            offset: { //margin-right 多少
+                type: [Number, String]
+            }
+        },
+        data (){
+            return {
+                gutter:0
             }
         }
     }
@@ -18,14 +29,21 @@
 <style scoped lang="scss">
     .col {
         height: 50px;
-        background-color: #eee;
+        /*background-color: #eee;*/
         width: 50%;
-        border: 1px solid red;
+        /*border: 1px solid red;*/
 
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-right: ($n / 24) * 100%;
             }
         }
     }
--- src/Row.vue
+++ src/Row.vue
@@ -1,17 +1,32 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2}+'px'">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "g-row"
+        name: "g-row",
+        props: {
+            gutter: { //平均间距大小
+                type: [Number, String]
+            }
+        },
+        created(){
+            console.log(this.$children)
+        },
+        mounted() {
+            console.log(this.$children)
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter //将gutter传到每个子组件中
+            })
+        }
     }
 </script>
 
 <style scoped lang="scss">
     .row {
         display: flex;
+        margin: 0 -10px;
     }
 </style>
