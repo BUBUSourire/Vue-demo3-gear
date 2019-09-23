e1b302154f0340c99eddf838c472b9c4349e3810
Sourire <953810390@qq.com>
2019-09-23 10:27:26 +0800

    添加简单动画

--- src/Sider.vue
+++ src/Sider.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="sider">
-        <slot></slot>
-    </div>
+    <transition name="fade">
+        <div class="sider" v-if="active">
+            <slot></slot>
+            <button @click="active = false">点击</button>
+        </div>
+    </transition>
 </template>
 
 <script>
     export default {
-        name: "g-sider"
+        name: "g-sider",
+        data() {
+            return {
+                active: true
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    .sider {
+        width: 150px;
+    }
 
+    .fade-enter-active, .fade-leave-active {
+        transition: all 1s;
+    }
+
+    .fade-enter, .fade-leave-to {
+       margin-left: -150px;
+    }
 </style>
