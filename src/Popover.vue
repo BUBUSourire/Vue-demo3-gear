9fbd61054bdc6e9046ad32403764fa91f450824c
Sourire <953810390@qq.com>
2019-10-04 23:26:35 +0800

    popover 功能基本完成

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div class="popContentStyle" v-if="showing" ref="contentWrapper">
-            <slot name="popContent"></slot>
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+            <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
             <slot></slot>
@@ -14,25 +14,42 @@
         name: "g-popover",
         data() {
             return {
-                showing: false
+                visible: false
             }
         },
         methods: {
-            xxx(e) {
-                // e.stopPropagation()
-                this.showing = !this.showing
-                if (this.showing === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper) //解决overflow：hidden的bug
-                        let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        let eventHandler = () => {
-                            this.showing = false
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            onClickDocument(e){
+                if (this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target ||
+                        this.$refs.contentWrapper.contains(e.target))) {
+                } else {
+                    this.close()
+                }
+            },
+            open() {
+                this.visible = true
+                setTimeout(() => {
+                    this.positionContent()
+                    document.addEventListener('click', this.onClickDocument)
+                }, 0)
+            },
+            close() {
+                this.visible = false
+                document.removeEventListener('click', this.onClickDocument)
+            },
+            onClick(e) {
+                if (this.$refs.triggerWrapper.contains(e.target)) {
+                    if (this.visible === true) {
+                        this.close()
+                    }else{
+                        this.open()
+                    }
                 }
             }
         }
@@ -42,16 +59,16 @@
 <style scoped lang="scss">
     .popover {
         display: inline-block;
-        vertical-align: bottom;
+        vertical-align: top;
         position: relative;
-        margin: 50px 0;
     }
 
-    .popContentStyle {
+    .content-wrapper {
         position: absolute;
         border: 1px solid red;
-        box-shadow: 0 0 3px #333333;
+        box-shadow: 0 0 3px #666666;
         transform: translateY(-100%);
     }
+
 
 </style>
