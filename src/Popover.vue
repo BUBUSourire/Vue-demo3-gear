195d4fa0fdb9d5c179820e25e5abb82de60bb4d9
Sourire <953810390@qq.com>
2019-10-04 14:20:11 +0800

    实现最简单的 popover click-showing

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="popContentStyle" v-if="showing">
+    <div class="popover" @click.stop="xxx">
+        <div class="popContentStyle" v-if="showing" @click.stop>
             <slot name="popContent"></slot>
         </div>
-        <slot></slot>
+            <slot></slot>
     </div>
 </template>
 
@@ -16,8 +16,19 @@
             }
         },
         methods: {
-            xxx() {
+            xxx(e) {
+                e.stopPropagation()
                 this.showing = !this.showing
+                if (this.showing === true) {
+                    let eventHandler = () => {
+                        this.showing = false
+                        console.log('document隐藏')
+                        document.removeEventListener('click', eventHandler)
+                    }
+                    document.addEventListener('click', eventHandler)
+                }else{
+                    console.log('组件自身隐藏')
+                }
             }
         }
     }
@@ -28,10 +39,11 @@
         display: inline-block;
         vertical-align: bottom;
         position: relative;
+        margin: 50px 0;
 
         .popContentStyle {
             position: absolute;
-            bottom: 10px;
+            bottom: 20px;
             border: 1px solid red;
             box-shadow: 0 0 3px #333333;
         }
