bcba61d06ac2a260bd4f7ebd8f5ab04fa568677b
Sourire <953810390@qq.com>
2019-10-09 14:18:06 +0800

    添加single选项 可弹出一个还是多个

--- src/Collapse-item.vue
+++ src/Collapse-item.vue
@@ -1,53 +1,76 @@
 <template>
     <div class="collapseItem">
-            <div class="title" @click="open=!open">
-                {{title}}
-            </div>
-            <div class="content" v-if="open">
-                <slot></slot>
-            </div>
+        <div class="title" @click="toggle">
+            {{title}}
+        </div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "g-collapse-item",
-        props:{
-           title:{
-               type:String,
-               require:true
-           }
+        props: {
+            title: {
+                type: String,
+                require: true
+            }
         },
-        data(){
-            return{
-                open:false
+        data() {
+            return {
+                open: false
+            }
+        },
+        inject: ['eventBus'],
+        mounted() {
+            this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+                if (vm !== this) {
+                    this.close()
+                }
+            })
+        },
+        methods: {
+            toggle() {
+                if (this.open) {
+                    this.open = false
+                } else {
+                    this.open = true
+                    this.eventBus && this.eventBus.$emit('update:selected', this)
+                }
+            },
+            close() {
+                this.open = false
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-    $border-radius:3px;
+    $border-radius: 3px;
     .collapseItem {
-        >.title {
+        > .title {
             border: 1px solid #cccccc;
-            border-collapse:collapse;
+            border-collapse: collapse;
             margin: -1px;
             min-height: 32px;
             display: flex;
             align-items: center;
             padding: 0 8px;
         }
+
         &:first-child {
-            >.title {
-                border-top-left-radius:  $border-radius;
-                border-top-right-radius:  $border-radius;
+            > .title {
+                border-top-left-radius: $border-radius;
+                border-top-right-radius: $border-radius;
             }
         }
-        &:last-child{
-            >.title:last-child {
-                border-bottom-left-radius:  $border-radius;
-                border-bottom-right-radius:  $border-radius;
+
+        &:last-child {
+            > .title:last-child {
+                border-bottom-left-radius: $border-radius;
+                border-bottom-right-radius: $border-radius;
             }
         }
     }
--- src/Collapse.vue
+++ src/Collapse.vue
@@ -5,15 +5,35 @@
 </template>
 
 <script>
+    import Vue from 'vue'
+
     export default {
-        name: "g-collapse"
+        name: "g-collapse",
+        props:{
+            single:{
+                type:Boolean,
+                default:false
+            }
+        },
+        data() {
+            return {
+                eventBus: new Vue()
+            }
+        },
+        provide() {
+            if(this.single){
+                return {
+                    eventBus: this.eventBus
+                }
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
     .collapse {
-        border:1px solid #ccc;
+        border: 1px solid #ccc;
         border-radius: 3px;
-        border-collapse:collapse;
+        border-collapse: collapse;
     }
 </style>
